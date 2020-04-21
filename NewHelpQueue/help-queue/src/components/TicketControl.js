a7e1225fe34cbe75dcc9386fcb6462abebd09254
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-21 11:48:19 -0700

    update state to have a counter, update render method to conditionally display components based on counter
    Co-authored-by: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>
    Co-authored-by: Miles Bradt <smbradtmichael@gmail.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>

--- NewHelpQueue/help-queue/src/components/TicketControl.js
+++ NewHelpQueue/help-queue/src/components/TicketControl.js
@@ -1,31 +1,46 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
+import Steps from './Steps';
+import Help from './Help';
+import FifteenMins from './FifteenMins';
 
 class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      // formVisibleOnPage: false,
+      counter: 0
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick = () => {
     this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
+      counter: prevState.counter + 1
     }));
   }
 
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (!this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />;
-      buttonText = "Return to Ticket List";
-    } else {
+    if (this.state.counter == 0) {
       currentlyVisibleState = <TicketList />;
       buttonText = "Add Ticket";
+    } else if (this.state.counter == 1) {
+      currentlyVisibleState = <Steps />;
+      buttonText = "Yes";
+    } else if (this.state.counter == 2) {
+      currentlyVisibleState = <Help />;
+      buttonText = "Yes";
+    } else if (this.state.counter == 3) {
+      currentlyVisibleState = <FifteenMins />;
+      buttonText = "Yes";
+    } else if (this.state.counter == 4) {
+      currentlyVisibleState = <NewTicketForm />;
+      this.state.counter = -1;
+      buttonText = "Return to Ticket List";
     }
     return (
       <React.Fragment>
